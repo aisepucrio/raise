eb1d0109f5502c441082baed0b87a0b7d405a226
Robbie Carvalho <130287531+R0bbyt3@users.noreply.github.com>
2026-02-24 17:52:17 -0300

    APIMINER-638 Pequena personalização na página de não encontrado

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -1,8 +1,8 @@
 export default function NotFound() {
   return (
-    <div className="space-y-2">
+    <div className="space-y-2 h-full m-auto text-center">
       <h1 className="text-2xl font-bold">404</h1>
-      <p className="opacity-80">Página não encontrada.</p>
+      <p className="opacity-80">Not Found.</p>
     </div>
   );
 }
