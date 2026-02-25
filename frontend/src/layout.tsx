ac8e72f5d6ca5d91af6b1c0318ad1148dfeebb9d
Robbie Carvalho <130287531+R0bbyt3@users.noreply.github.com>
2026-02-25 10:23:27 -0300

    APIMINER-638 Fazendo componentes dentro do layout não "vazararem" para fora

--- frontend/src/layout.tsx
+++ frontend/src/layout.tsx
@@ -3,11 +3,11 @@ import Sidebar from "./components/sidebar/Sidebar";
 
 export default function Layout() {
   return (
-    <div className="min-h-dvh md:flex">
+    <div className="flex h-dvh flex-col overflow-hidden md:flex-row">
       <Sidebar />
 
-      <main className="min-w-0 flex-1 p-6">
-        <div className="max-w-5xl">
+      <main className="min-h-0 min-w-0 max-w-full flex-1 overflow-x-hidden overflow-y-auto p-6">
+        <div className="w-full min-w-0 max-w-full">
           <Outlet />
         </div>
       </main>
