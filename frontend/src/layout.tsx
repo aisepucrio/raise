4f9b56a5719b753965387cfe0f17d48f32fd3693
Robbie Carvalho <130287531+R0bbyt3@users.noreply.github.com>
2026-02-25 09:09:53 -0300

    APIMINER-638 Renomeando pasta Sidebar -> sidebar

--- frontend/src/layout.tsx
+++ frontend/src/layout.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Sidebar from "./components/Sidebar/Sidebar";
+import Sidebar from "./components/sidebar/Sidebar";
 
 export default function Layout() {
   return (
