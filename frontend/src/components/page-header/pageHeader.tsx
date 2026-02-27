e8321ded44a6404c2211522fb6e2732cafa1f19d
Robbie Carvalho <130287531+R0bbyt3@users.noreply.github.com>
2026-02-27 11:37:44 -0300

    APIMINER-638 Pequena mudança

--- frontend/src/components/page-header/pageHeader.tsx
+++ frontend/src/components/page-header/pageHeader.tsx
@@ -5,7 +5,7 @@ type PageHeaderProps = {
 
 export function PageHeader({ title, subtitle }: PageHeaderProps) {
   return (
-    <header className="mb-8 border-b-2 border-(--color-secondary-soft) pb-5">
+    <header className="mb-2 border-b-2 border-(--color-secondary-soft) pb-5">
       <div className="space-y-2">
         <h1 className="text-3xl font-bold tracking-tight md:text-4xl">
           {title}
