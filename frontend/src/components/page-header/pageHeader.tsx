6cc2ac1bd66f9f5c00b9ead56e5684f25a14e01f
Robbie Carvalho <130287531+R0bbyt3@users.noreply.github.com>
2026-03-01 11:47:00 -0300

    APIMINER-638 Pequenas mudanças gerais no layout

--- frontend/src/components/page-header/pageHeader.tsx
+++ frontend/src/components/page-header/pageHeader.tsx
@@ -5,12 +5,12 @@ type PageHeaderProps = {
 
 export function PageHeader({ title, subtitle }: PageHeaderProps) {
   return (
-    <header className="mb-2 border-b-2 border-(--color-secondary-soft) pb-5">
-      <div className="space-y-2">
-        <h1 className="text-3xl font-bold tracking-tight md:text-4xl">
+    <header className="mb-3 border-b-2 border-(--color-secondary-soft) pb-3">
+      <div className="space-y-1">
+        <h1 className="text-2xl font-bold tracking-tight md:text-3xl">
           {title}
         </h1>
-        <p className="text-base text-(--color-secondary-muted) md:text-lg">
+        <p className="text-base text-(--color-secondary-muted) md:text-md">
           {subtitle}
         </p>
       </div>
--- frontend/src/layout.tsx
+++ frontend/src/layout.tsx
@@ -6,7 +6,7 @@ export default function Layout() {
     <div className="flex h-dvh flex-col overflow-hidden md:flex-row">
       <Sidebar />
 
-      <main className="min-h-0 min-w-0 max-w-full flex-1 overflow-x-hidden overflow-y-auto p-6">
+      <main className="min-h-0 min-w-0 max-w-full flex-1 overflow-x-hidden overflow-y-auto p-4">
         <div className="h-full min-h-0 w-full min-w-0 max-w-full">
           <Outlet />
         </div>
