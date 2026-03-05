5cd12687170e59fa8d36d5714a2a608bd4da45c6
Robbie Carvalho <130287531+R0bbyt3@users.noreply.github.com>
2026-03-05 09:02:27 -0300

    APIMINER-638 Pequena mudança na organização da file endpoints

--- frontend/src/data/api/endpoints.ts
+++ frontend/src/data/api/endpoints.ts
@@ -10,18 +10,21 @@ export const endpoints = {
   // ==> OVERVIEW
   dashboard: (source: Source) => `/api/${source}/dashboard`,
   dashboardGraph: (source: Source) => `/api/${source}/dashboard/graph`,
+
+  // ==> DATE RANGE (OVERVIEW / PREVIEW)
   dateRange: (source: Source) => `/api/${source}/date-range`,
+
+  // ==> COLLECT
+  collect: (source: Source) => `/api/${source}/collect/`,
+
+  // HARDCODE TEMPORARIO: O --StackOverflow-- AINDA USA /COLLECT/ADVANCED POR COMPATIBILIDADE COM A IMPLEMENTACAO LEGADA.
+  // FUTURO: MERGEAR COM /COLLECT USANDO APENAS PAYLOAD.
+  collectAdvanced: (source: Source) => `/api/${source}/collect/advanced/`,
 
   // ==> PREVIEW
   previewList: <S extends Source>(source: S, section: SectionBySource[S]) =>
     `/api/${source}/${section}`,
   export: (source: Source) => `/api/${source}/export/`,
-
-  // ==> COLLECT
-  collect: (source: Source) => `/api/${source}/collect/`,
-  // HARDCODE TEMPORARIO: O SO AINDA USA /COLLECT/ADVANCED POR COMPATIBILIDADE COM A IMPLEMENTACAO LEGADA.
-  // FUTURO: MERGEAR COM /COLLECT USANDO APENAS PAYLOAD.
-  collectAdvanced: (source: Source) => `/api/${source}/collect/advanced/`,
 
   // ==> JOBS
   jobs: () => `/api/jobs/`,
